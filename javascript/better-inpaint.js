d6f91c6c41384ec88fe4f55c525bf8e62c100671
JohnWL <34081873+John-WL@users.noreply.github.com>
2024-05-08 22:20:30 -0400

    gradio components and events, zoom

--- javascript/better-inpaint.js
+++ javascript/better-inpaint.js
@@ -1,4 +1,10 @@
-(async () => {
+// The definition of these functions are in the IIFE. 
+// The point is to keep a clean global scope. 
+// Only these functions will be accessible globaly. 
+let better_inpaint_update_all;
+
+
+(() => {
     const BETTER_INPAINT_POLLING_TIMEOUT = 500;
 
 
@@ -30,6 +36,7 @@
         try {
             return [
                 document.querySelector('#img2img_inpaint_full_res').parentElement.parentElement.parentElement,
+                // document.querySelector('#img2img_better_inpaint_image'),
             ];
         }
         catch {
@@ -69,12 +76,6 @@
     
     function populateRoot(root) {
         const betterInpaint = document.createElement('better-inpaint');
-        betterInpaint.addEventListener("region", event => {
-            const updatedRegion = event.region;
-        });
-        betterInpaint.addEventListener("mask", event => {
-            const updatedMask = event.mask;
-        });
         root.appendChild(betterInpaint);
     }
 
@@ -88,8 +89,17 @@
         connectedCallback() {
             this.container = document.createElement("div");
             this.appendChild(this.container);
+            this.container.style.position = "relative";
+            this.container.style.overflow = "hidden";
             this.container.style.height = "800px";
             this.container.addEventListener("wheel", this.handleWheel);
+
+            this.viewport = document.createElement("better-inpaint-viewport");
+            this.container.appendChild(this.viewport);
+            this.viewport.style.width = "100%";
+            this.viewport.style.height = "100%";
+            this.viewport.style.position = "absolute";
+
             console.log("[sd-webui-better-inpaint] Root component created");
         }
 
@@ -101,9 +111,241 @@
 
         handleWheel(event) {
             event.preventDefault();
+            const scrollAmount = event.deltaY > 0 ? 1 : -1;
+            const clientRect = this.container.getBoundingClientRect();
+            const mouseX = 100 * (event.clientX - clientRect.left) / clientRect.width;
+            const mouseY = 100 * (event.clientY - clientRect.top) / clientRect.height;
+            const [cursorOffsetX, cursorOffsetY] = [
+                mouseX - this.viewport.offset[0],
+                mouseY - this.viewport.offset[1],
+            ];
+            const previousScale = this.viewport.scale;
+            if(scrollAmount < 0) {
+                this.viewport.scale *= 1.1;
+            }
+            if(scrollAmount > 0) {
+                this.viewport.scale /= 1.1;
+            }
+            const actualScaleApplied = this.viewport.scale / previousScale;
+            this.viewport.offset = [
+                this.viewport.offset[0] + cursorOffsetX * (1 - actualScaleApplied),
+                this.viewport.offset[1] + cursorOffsetY * (1 - actualScaleApplied),
+            ]
+        }
+    }
+
+
+    class ViewportElement extends HTMLElement {
+        constructor() {
+            super();
+            this._scale = 100;
+            this._offset = [0, 0];
+        }
+        
+        connectedCallback() {
+            this.image = document.createElement("better-inpaint-viewport-image");
+            this.appendChild(this.image);
+            this.image.style.position = "absolute";
+            this.image.style.width = "100%";
+
+            this.mask = document.createElement("better-inpaint-viewport-mask");
+            this.appendChild(this.mask);
+            this.mask.style.position = "absolute";
+            this.mask.style.width = "100%";
+        }
+
+        disconnectedCallback() {
+
+        }
+        
+        get scale() {
+            return this._scale;
+        }
+
+        set scale(value) {
+            if(value > 1000) return;
+            if(value < 20) return;
+
+            this._scale = value;
+            this.style.width = `${value}%`;
+            this.style.height = `${value}%`;
+        }
+
+        get offset() {
+            return this._offset;
+        }
+
+        set offset(value) {
+            if(value[0] > 80) value[0] = 80;
+            if(value[0] < 20 - this.scale) value[0] = 20 - this.scale;
+            if(value[1] > 80) value[1] = 80;
+            if(value[1] < 20 - this.scale) value[1] = 20 - this.scale;
+
+            this._offset = value;
+            this.style.left = `${value[0]}%`;
+            this.style.top = `${value[1]}%`;
+        }
+    }
+
+
+    class ViewportImageElement extends HTMLElement {
+        constructor() {
+            super();
+        }
+
+        connectedCallback() {
+            this.image = document.createElement("img");
+            this.appendChild(this.image);
+            this.image_upload_observer = watch_gradio_image("#img2img_better_inpaint_image_upload", src => {
+                this.image.src = src;
+                const fn = () => {
+                    compute_cropped_images()
+                        .then(request_update_cropped_image)
+                        .catch(console.log);
+                };
+                this.image.onload = fn;
+                if(src === "") fn();
+            });
+            this.image.setAttribute("data-type", "full");
+            this.image.style.position = "absolute";
+            this.image.style.width = "100%";
+
+            this.croppedImage = document.createElement("img");
+            this.appendChild(this.croppedImage);
+            this.croppedImage.setAttribute("data-type", "cropped");
+            this.croppedImage.style.display = "none";
+        }
+
+        disconnectedCallback() {
+            this.image_upload_observer.disconnect();
+        }
+    }
+
+
+    class ViewportMaskElement extends HTMLElement {
+        constructor() {
+            super();
+        }
+
+        connectedCallback() {
+            this.maskCanvas = document.createElement("canvas");
+            this.appendChild(this.maskCanvas);
+
+            this.mask = document.createElement("img");
+            this.appendChild(this.mask);
+            this.mask.setAttribute("data-type", "full");
+            this.mask.style.display = "none";
+
+            this.croppedMask = document.createElement("img");
+            this.appendChild(this.croppedMask);
+            this.croppedMask.setAttribute("data-type", "cropped");
+            this.croppedMask.style.display = "none";
+        }
+
+        disconnectedCallback() {
+            this.image_upload_observer.disconnect();
         }
     }
 
 
     customElements.define('better-inpaint', BetterInpaintElement);
+    customElements.define('better-inpaint-viewport', ViewportElement);
+    customElements.define('better-inpaint-viewport-image', ViewportImageElement);
+    customElements.define('better-inpaint-viewport-mask', ViewportMaskElement);
+
+
+    function watch_gradio_image(id, modifiedCallback) {
+        const targetNode = document.querySelector(id);
+        const config = { childList: true, subtree: true, attributes: true, attributeFilter: ['src'] };
+        const callback = function(mutationsList, _) {
+            for(const mutation of mutationsList) {
+                if(mutation.type === 'childList') {
+                    mutation.addedNodes.forEach(node => {
+                        if(node.tagName === 'IMG') modifiedCallback(node.src);
+                    });
+                    mutation.removedNodes.forEach(node => {
+                        if(node.tagName === 'IMG')  modifiedCallback("");
+                    });
+                }
+                else if(mutation.type === 'attributes' && mutation.attributeName === 'src') {
+                    modifiedCallback(mutation.target.src);
+                }
+            }
+        };
+        const observer = new MutationObserver(callback);
+        observer.observe(targetNode, config);
+        return observer;
+    }
+
+
+    function compute_cropped_images() {
+        return new Promise((resolve, reject) => {
+            const maskCanvas = document.querySelector('better-inpaint-viewport-mask > canvas');
+            const mask = document.querySelector('better-inpaint-viewport-mask > img[data-type="full"]');
+            const croppedMask = document.querySelector('better-inpaint-viewport-mask > img[data-type="cropped"]');
+            const image = document.querySelector('better-inpaint-viewport-image > img[data-type="full"]');
+            const croppedImage = document.querySelector('better-inpaint-viewport-image > img[data-type="cropped"]');
+
+            const rect = undefined; // TODO: Implement rect selection
+
+            if(image.naturalWidth === 0 || image.naturalHeight === 0) {
+                mask.src = "";
+                croppedMask.src = "";
+                croppedImage.src = "";
+                resolve();
+                return;
+            }
+
+            maskCanvas.width = image.naturalWidth;
+            maskCanvas.height = image.naturalHeight;
+            mask.src = maskCanvas.toDataURL("image/png");
+            mask.onload = () => {
+                Promise.all([
+                    crop_image(image, croppedImage, rect),
+                    crop_image(mask, croppedMask, rect),
+                ])
+                    .then(resolve)
+                    .catch(reject);
+            };
+        });
+    }
+
+
+    function crop_image(initialImage, imageResult, rect) {
+        return new Promise((resolve, reject) => {
+            const imageElement = document.createElement("img");
+            imageElement.src = initialImage.src;
+            imageElement.onload = () => {
+                if(rect === undefined) rect = [0, 0, imageElement.width, imageElement.height];
+                const canvas = document.createElement("canvas");
+                const ctx = canvas.getContext("2d");
+                const [x, y, w, h] = rect;
+                canvas.width = w;
+                canvas.height = h;
+                ctx.drawImage(imageElement, x, y, w, h, 0, 0, w, h);
+                imageResult.onload = resolve;
+                imageResult.onerror = reject;
+                imageResult.src = canvas.toDataURL("image/png");
+            };
+            imageElement.onerror = reject;
+        });
+    }
+
+
+    function request_update_cropped_image() {
+        document.querySelector("#img2img_better_inpaint_update_all").click();
+    }
+
+
+    function _better_inpaint_update_all() {
+        // ouputs: [Mask, Cropped image, Cropped mask, (Resize to tab, Resize to sliders) -> later]
+        const image = document.querySelector('better-inpaint-viewport-image > img[data-type="full"]');
+        const croppedImage = document.querySelector('better-inpaint-viewport-image > img[data-type="cropped"]');
+        const mask = document.querySelector('better-inpaint-viewport-mask > img[data-type="full"]');
+        const croppedMask = document.querySelector('better-inpaint-viewport-mask > img[data-type="cropped"]');
+        
+        if(image.naturalWidth === 0 || image.naturalHeight === 0) return [null, null, null];
+        return [mask.src, croppedImage.src, croppedMask.src];
+    }
+    better_inpaint_update_all = _better_inpaint_update_all;
 })();
--- lib_better_inpaint/ui.py
+++ lib_better_inpaint/ui.py
@@ -7,23 +7,44 @@ class BetterInpaintTab(OperationMode):
     requested_elem_ids = ["img2img_mask_alpha"]
 
     def image_components(self):
-        self.inpaint_alt_component = gr.Image(visible=False, label="Image", source="upload", interactive=True, type="pil", elem_id="img2img_better_inpaint_image")
-        self.inpaint_mask_component = gr.Image(visible=False, label="Mask", interactive=True, type="pil", elem_id="img2img_better_inpaint_mask")
-        return self.inpaint_alt_component, self.inpaint_mask_component
+        self.inpaint_img_component = gr.Image(visible=True, label="Cropped image", interactive=False, type="pil", elem_id="img2img_better_inpaint_image")
+        self.inpaint_img_component.unrender()
+        self.inpaint_mask_component = gr.Image(visible=True, label="Cropped mask", interactive=False, type="pil", elem_id="img2img_better_inpaint_mask")
+        return self.inpaint_img_component, self.inpaint_mask_component
 
     def tab(self):
         with gr.TabItem(label='Better inpaint') as self.tab:
+            self.update_all = gr.Button(value="", visible=False, elem_id="img2img_better_inpaint_update_all")
+            with gr.Row():
+                self.inpaint_img_component.render()
+                self.inpaint_img_upload = gr.Image(label="Image", source="upload", interactive=True, type="pil", elem_id="img2img_better_inpaint_image_upload")
+                self.inpaint_mask_upload = gr.Image(visible=True, label="Mask", interactive=False, type="pil", elem_id="img2img_better_inpaint_mask_upload")
+            
             self.better_inpaint_root = gr.HTML("", elem_id="img2img_better_inpaint_root")
 
-    def section(self, components):
+    def section(self, components: list):
         self.mask_alpha = components["img2img_mask_alpha"]
 
     def gradio_events(self, selected: gr.Checkbox):
         self._update_sliders_visibility(selected)
+        self._update_all_image_components(selected)
 
-    def _update_sliders_visibility(self, selected):
+    def _update_sliders_visibility(self, selected: gr.Checkbox):
         selected.change(
             fn=lambda is_this_tab_selected: gr.update(visible=False) if is_this_tab_selected else gr.update(),
             inputs=[selected],
             outputs=[self.mask_alpha],
+        )
+    
+    def _update_all_image_components(self, selected: gr.Checkbox):
+        self.update_all.click(
+            fn=None,
+            inputs=[],
+            outputs=[self.inpaint_mask_upload, self.inpaint_img_component, self.inpaint_mask_component],
+            _js="better_inpaint_update_all"
+        )
+        self.inpaint_mask_upload.change(
+            fn=lambda rgb_mask: gr.update(value=None if rgb_mask is None else rgb_mask.convert("L")),
+            inputs=[self.inpaint_mask_upload],
+            outputs=[self.inpaint_mask_component],
         )
