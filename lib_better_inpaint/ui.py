d350ce8b810b8397b6a149e9dcdef1e8559a9c6c
JohnWL <34081873+John-WL@users.noreply.github.com>
2024-05-11 15:41:05 -0400

    Pin only masked radio

--- lib_better_inpaint/ui.py
+++ lib_better_inpaint/ui.py
@@ -4,7 +4,7 @@ from sdwi2iextender import OperationMode
 
 class BetterInpaintTab(OperationMode):
     show_inpaint_params = True
-    requested_elem_ids = ["img2img_mask_alpha"]
+    requested_elem_ids = ["img2img_mask_alpha", "img2img_inpaint_full_res"]
 
     def image_components(self):
         self.inpaint_img_component = gr.Image(label="Cropped image", interactive=False, type="pil", elem_id="img2img_better_inpaint_image")
@@ -29,9 +29,12 @@ class BetterInpaintTab(OperationMode):
 
     def section(self, components: list):
         self.mask_alpha = components["img2img_mask_alpha"]
+        self.inpaint_full_res = components["img2img_inpaint_full_res"]
+        self.previous_inpaint_full_res = gr.State(0)
 
     def gradio_events(self, selected: gr.Checkbox):
         self._update_sliders_visibility(selected)
+        self._toggle_only_masked(selected)
         self._update_all_image_components(selected)
 
     def _update_sliders_visibility(self, selected: gr.Checkbox):
@@ -39,6 +42,24 @@ class BetterInpaintTab(OperationMode):
             fn=lambda is_this_tab_selected: gr.update(visible=False) if is_this_tab_selected else gr.update(),
             inputs=[selected],
             outputs=[self.mask_alpha],
+        )
+    
+    def _toggle_only_masked(self, selected: gr.Checkbox):
+        def handle_only_masked_visibility(enabled, previous_choice_id):
+            choices = [value for _, value in self.inpaint_full_res.choices]
+            choice_id = 0 if enabled else previous_choice_id
+            return choices[choice_id]
+
+        selected.change(
+            fn=handle_only_masked_visibility,
+            inputs=[selected, self.previous_inpaint_full_res],
+            outputs=[self.inpaint_full_res],
+        )
+
+        self.inpaint_full_res.change(
+            fn=lambda enabled, choice_id, previous_choice_id: previous_choice_id if enabled else choice_id,
+            inputs=[selected, self.inpaint_full_res, self.previous_inpaint_full_res],
+            outputs=[self.previous_inpaint_full_res],
         )
     
     def _update_all_image_components(self, selected: gr.Checkbox):
